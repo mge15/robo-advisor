8276e44d5c9f7cbdbb470d29da36e0f2f43015a1
mge15 <77738774+mge15@users.noreply.github.com>
2021-02-25 13:07:50 -0500

    Writing daily prices to csv file

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -53,9 +53,21 @@ human_friendly_timestamp = timestamp.strftime("%Y-%m-%d %H:%M:%S")
 
 csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
 
+csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
+
 with open(csv_file_path, "w") as csv_file:
-    writer = csv.DictWriter(csv_file, filednames=["city", "name"])
+    writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
     writer.writeheader()
+    for date in dates:
+        daily_prices = parsed_response["Time Series (Daily)"][date]
+        writer.writerow({
+            "timestamp": date,
+            "open": daily_prices["1. open"],
+            "high": daily_prices["2. high"],
+            "low": daily_prices["3. low"],
+            "close": daily_prices["4. close"],
+            "volume": daily_prices["5. volume"]
+        })
 
 
 print("-------------------------")
