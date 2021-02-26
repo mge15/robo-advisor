c7e06c643db2fc53a7a9381d5f8a192b3579f3c1
mge15 <77738774+mge15@users.noreply.github.com>
2021-02-25 21:00:35 -0500

    Allowing input for more than one stock

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -20,89 +20,95 @@ def to_usd(my_price):
     return f"${my_price:,.2f}"  # > $12,000.71
 
 load_dotenv()
+symbol_list = []
 
 while True:
-    symbol = input("Please input one stock or cryptocurrency symbol: ")
-    if symbol.isalpha() and len(symbol) < 5:
+    symbol = input("Please input a stock or cryptocurrency symbol, or 'DONE' if there are no more stocks you would like to view: ")
+    if symbol.upper() == "DONE":
         break
+    if symbol.isalpha() and len(symbol) < 5:
+        symbol_list.append(symbol)
     else:
         print("Expecting a properly-formed stock symbol like 'MSFT'. Please try again.")
 
 api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
 
-request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
+for sym in symbol_list:
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={sym}&apikey={api_key}"
+    while True:
+        response = requests.get(request_url)
+        parsed_response = json.loads(response.text)
+        if "Error Message" in parsed_response.keys():
+            print("Sorry, couldn't find any trading data for that stock symbol")
+            exit()
+        else:
+            break
+    last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
-while True:
-    response = requests.get(request_url)
-    parsed_response = json.loads(response.text)
-    if "Error Message" in parsed_response.keys():
-        print("Sorry, couldn't find any trading data for that stock symbol")
-        exit()
-    else:
-        break
+    # assumes that first day is on top
+    dates = list(parsed_response["Time Series (Daily)"].keys())
+    latest_day = dates[0]
 
-last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+    latest_close = float(parsed_response["Time Series (Daily)"][latest_day]["4. close"])
 
-# assumes that first day is on top
-dates = list(parsed_response["Time Series (Daily)"].keys())
-latest_day = dates[0]
+    # get high price form each day
+    high_prices = []
+    low_prices = []
 
-latest_close = float(parsed_response["Time Series (Daily)"][latest_day]["4. close"])
-
-# get high price form each day
-high_prices = []
-low_prices = []
-
-for date in dates:
-    high_price = float(parsed_response["Time Series (Daily)"][date]["2. high"])
-    high_prices.append(high_price)
-    low_price = float(parsed_response["Time Series (Daily)"][date]["3. low"])
-    low_prices.append(low_price)
-
-# maximum of all of the high prices
-recent_high = max(high_prices)
-
-# minimum of all the low prices
-recent_low = min(low_prices)
-
-timestamp = datetime.now()
-human_friendly_timestamp = timestamp.strftime("%Y-%m-%d %H:%M:%S")
-
-
-csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
-
-csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
-
-with open(csv_file_path, "w") as csv_file:
-    writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
-    writer.writeheader()
     for date in dates:
-        daily_prices = parsed_response["Time Series (Daily)"][date]
-        writer.writerow({
-            "timestamp": date,
-            "open": daily_prices["1. open"],
-            "high": daily_prices["2. high"],
-            "low": daily_prices["3. low"],
-            "close": daily_prices["4. close"],
-            "volume": daily_prices["5. volume"]
-        })
+        high_price = float(parsed_response["Time Series (Daily)"][date]["2. high"])
+        high_prices.append(high_price)
+        low_price = float(parsed_response["Time Series (Daily)"][date]["3. low"])
+        low_prices.append(low_price)
+
+    # maximum of all of the high prices
+    recent_high = max(high_prices)
+
+    # minimum of all the low prices
+    recent_low = min(low_prices)
+
+    timestamp = datetime.now()
+    human_friendly_timestamp = timestamp.strftime("%Y-%m-%d %H:%M:%S")
 
 
-print("-------------------------")
-print("SELECTED SYMBOL: ", symbol.upper())
-print("-------------------------")
-print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: ", human_friendly_timestamp)
-print("-------------------------")
-print("LATEST DAY: ", last_refreshed)
-print("LATEST CLOSE: ", to_usd(latest_close))
-print("RECENT HIGH: ", to_usd(recent_high))
-print("RECENT LOW: ", to_usd(recent_low))
-print("-------------------------")
-print("RECOMMENDATION: BUY!") #need to still do this
-print("RECOMMENDATION REASON: TODO")
-print("-------------------------")
-print("WRITING DATA TO CSV: ", csv_file_path)
+    csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", f"prices_{sym}.csv")
+
+    csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
+
+    with open(csv_file_path, "w") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
+        writer.writeheader()
+        for date in dates:
+            daily_prices = parsed_response["Time Series (Daily)"][date]
+            writer.writerow({
+                "timestamp": date,
+                "open": daily_prices["1. open"],
+                "high": daily_prices["2. high"],
+                "low": daily_prices["3. low"],
+                "close": daily_prices["4. close"],
+                "volume": daily_prices["5. volume"]
+            })
+
+
+    print("-------------------------")
+    print("SELECTED SYMBOL: ", sym.upper())
+    print("-------------------------")
+    print("REQUESTING STOCK MARKET DATA...")
+    print("REQUEST AT: ", human_friendly_timestamp)
+    print("-------------------------")
+    print("LATEST DAY: ", last_refreshed)
+    print("LATEST CLOSE: ", to_usd(latest_close))
+    print("RECENT HIGH: ", to_usd(recent_high))
+    print("RECENT LOW: ", to_usd(recent_low))
+    print("-------------------------")
+    print("RECOMMENDATION: BUY!") #need to still do this
+    print("RECOMMENDATION REASON: TODO")
+    print("-------------------------")
+    print("WRITING DATA TO CSV: ", csv_file_path)
+    print("-------------------------")
+    print()
+    print()
+
 print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
