ef13729a16fee020d5bd3020473b5e3438c69a42
mge15 <77738774+mge15@users.noreply.github.com>
2021-03-04 15:07:18 -0500

    Fixing Validation Checks

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -5,7 +5,6 @@ import json
 import os
 import requests
 import plotly
-#import seaborn as sns
 import plotly.express as px
 from datetime import datetime
 from dotenv import load_dotenv
@@ -40,96 +39,94 @@ api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
 
 for sym in symbol_list:
     request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={sym}&apikey={api_key}"
-    while True:
-        response = requests.get(request_url)
-        parsed_response = json.loads(response.text)
-        if "Error Message" in parsed_response.keys():
-            print("Sorry, couldn't find any trading data for the", sym, "stock symbol")
-            exit()
-        else:
-            break
-    last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
-
-    # assumes that first day is on top
-    dates = list(parsed_response["Time Series (Daily)"].keys())
-    latest_day = dates[0]
-
-    latest_close = float(parsed_response["Time Series (Daily)"][latest_day]["4. close"])
-
-    # get high price from each day
-    high_prices = []
-    low_prices = []
-    late_closes = []
-    end = 0
-
-    for date in dates:
-        high_price = float(parsed_response["Time Series (Daily)"][date]["2. high"])
-        high_prices.append(high_price)
-        low_price = float(parsed_response["Time Series (Daily)"][date]["3. low"])
-        low_prices.append(low_price)
-        late_close = float(parsed_response["Time Series (Daily)"][date]["4. close"])
-        late_closes.append({"date": date, "stock price": late_close})
-
-    # creating data frame
-    line_df = DataFrame(late_closes)
-
-    # maximum of all of the high prices
-    recent_high = max(high_prices)
-
-    # minimum of all the low prices
-    recent_low = min(low_prices)
-
-    timestamp = datetime.now()
-    human_friendly_timestamp = timestamp.strftime("%Y-%m-%d %H:%M:%S")
-
-    if latest_close < (1.2 * recent_low):
-        recommendation = "Buy"
-        reason = "The current stock price is close to the recent low i.e. within 20%"
+    response = requests.get(request_url)
+    parsed_response = json.loads(response.text)
+    if "Error Message" in parsed_response.keys():
+        print()
+        print("Sorry, couldn't find any trading data for the", sym, "stock symbol")
+        print()
     else:
-        recommendation = "Don't Buy"
-        reason = "The current stock price is too high to consider buying"
+        last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
-    csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", f"prices_{sym}.csv")
+        # assumes that first day is on top
+        dates = list(parsed_response["Time Series (Daily)"].keys())
+        latest_day = dates[0]
 
-    csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
+        latest_close = float(parsed_response["Time Series (Daily)"][latest_day]["4. close"])
 
-    with open(csv_file_path, "w") as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
-        writer.writeheader()
+        # get high price from each day
+        high_prices = []
+        low_prices = []
+        late_closes = []
+        end = 0
+
         for date in dates:
-            daily_prices = parsed_response["Time Series (Daily)"][date]
-            writer.writerow({
-                "timestamp": date,
-                "open": daily_prices["1. open"],
-                "high": daily_prices["2. high"],
-                "low": daily_prices["3. low"],
-                "close": daily_prices["4. close"],
-                "volume": daily_prices["5. volume"]
-            })
+            high_price = float(parsed_response["Time Series (Daily)"][date]["2. high"])
+            high_prices.append(high_price)
+            low_price = float(parsed_response["Time Series (Daily)"][date]["3. low"])
+            low_prices.append(low_price)
+            late_close = float(parsed_response["Time Series (Daily)"][date]["4. close"])
+            late_closes.append({"date": date, "stock price": late_close})
+
+        # creating data frame
+        line_df = DataFrame(late_closes)
+
+        # maximum of all of the high prices
+        recent_high = max(high_prices)
+
+        # minimum of all the low prices
+        recent_low = min(low_prices)
+
+        timestamp = datetime.now()
+        human_friendly_timestamp = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+
+        if latest_close < (1.2 * recent_low):
+            recommendation = "Buy"
+            reason = "The current stock price is close to the recent low i.e. within 20%"
+        else:
+            recommendation = "Don't Buy"
+            reason = "The current stock price is too high to consider buying"
+
+        csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", f"prices_{sym}.csv")
+
+        csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
+
+        with open(csv_file_path, "w") as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
+            writer.writeheader()
+            for date in dates:
+                daily_prices = parsed_response["Time Series (Daily)"][date]
+                writer.writerow({
+                    "timestamp": date,
+                    "open": daily_prices["1. open"],
+                    "high": daily_prices["2. high"],
+                    "low": daily_prices["3. low"],
+                    "close": daily_prices["4. close"],
+                    "volume": daily_prices["5. volume"]
+                })
 
 
-    print("-------------------------")
-    print("SELECTED SYMBOL: ", sym.upper())
-    print("-------------------------")
-    print("REQUESTING STOCK MARKET DATA...")
-    print("REQUEST AT: ", human_friendly_timestamp)
-    print("-------------------------")
-    print("LATEST DAY: ", last_refreshed)
-    print("LATEST CLOSE: ", to_usd(latest_close))
-    print("RECENT HIGH: ", to_usd(recent_high))
-    print("RECENT LOW: ", to_usd(recent_low))
-    print("-------------------------")
-    print("RECOMMENDATION: ", recommendation)
-    print("RECOMMENDATION REASON: ", reason)
-    print("-------------------------")
-    print("WRITING DATA TO CSV: ", csv_file_path)
-    print("-------------------------")
-    print("Displaying Line Chart of Close Prices over Time in browser...")
-    #sns.lineplot(data=line_df, x="date", y="stock price")
-    graph = px.line(line_df, x="date", y="stock price", title=f"Stock Price over Time: {sym.upper()}")
-    graph.show()
-    print()
-    print()
+        print("-------------------------")
+        print("SELECTED SYMBOL: ", sym.upper())
+        print("-------------------------")
+        print("REQUESTING STOCK MARKET DATA...")
+        print("REQUEST AT: ", human_friendly_timestamp)
+        print("-------------------------")
+        print("LATEST DAY: ", last_refreshed)
+        print("LATEST CLOSE: ", to_usd(latest_close))
+        print("RECENT HIGH: ", to_usd(recent_high))
+        print("RECENT LOW: ", to_usd(recent_low))
+        print("-------------------------")
+        print("RECOMMENDATION: ", recommendation)
+        print("RECOMMENDATION REASON: ", reason)
+        print("-------------------------")
+        print("WRITING DATA TO CSV: ", csv_file_path)
+        print("-------------------------")
+        print("Displaying Line Chart of Close Prices over Time in browser for your consideration...")
+        graph = px.line(line_df, x="date", y="stock price", title=f"Stock Price over Time: {sym.upper()}")
+        graph.show()
+        print()
+        print()
 
 print("-------------------------")
 print("HAPPY INVESTING!")
